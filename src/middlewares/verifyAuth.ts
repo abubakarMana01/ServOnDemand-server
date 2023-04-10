1d8ce1db333f501793d610d31cd155ceb27613b5
abubakarMana01 <ym.abubakr7@gmail.com>
2023-04-10 16:49:06 +0100

    fix: error message fixes

--- src/middlewares/verifyAuth.ts
+++ src/middlewares/verifyAuth.ts
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 
 const verifyAuth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) return res.status(401).send("Access denied. No token provided");
+  if (!token) return res.status(401).json({ error: { message: "Access denied. No token provided" } });
 
   try {
     const user = await jwt.verify(token, `${process.env.JWT_SECRET}`);
     req.user = user;
     next();
   } catch (err: unknown) {
-    res.status(400).send({ error: "Invalid token." });
+    res.status(400).json({ error: { message: "Invalid token." } });
   }
 };
 
